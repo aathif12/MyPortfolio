5b927c60d2ced57d8c8ebd1b8e836755869581dc
aathif12 <aathifahamad4@gmail.com>
2024-06-23 20:03:10 +0530

    Api

--- Portfolio/src/Components/Contact.jsx
+++ Portfolio/src/Components/Contact.jsx
@@ -1,28 +1,71 @@
+import React, { useState } from "react";
+
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "8be8b03d-4e14-48f5-ad70-bb121f90f214");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    } catch (error) {
+      console.log("Fetch error", error);
+      setResult("There was an error submitting the form.");
+    }
+  };
+
   return (
     <section className="contact" id="contact">
       <h2 className="heading">
         Contact<span> Me</span>
       </h2>
-      <form action="#">
+      <form action="#" onSubmit={onSubmit}>
         <div className="input-box">
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email Address" />
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            required
+          />
         </div>
         <div className="input-box">
-          <input type="text" placeholder="Mobile Number" />
-          <input type="text" placeholder="Email Subject" />
+          <input type="text" name="phone" placeholder="Mobile Number" />
+          <input
+            type="text"
+            name="subject"
+            placeholder="Email Subject"
+            required
+          />
         </div>
         <textarea
-          name=""
-          id=""
+          name="message"
           cols={30}
           rows={10}
           placeholder="Your Message"
+          required
         ></textarea>
-        <input type="submit" value={"Send Message"} className="btn" />
+        <input type="submit" value="Send Message" className="btn" />
       </form>
     </section>
   );
 };
+
 export default Contact;
