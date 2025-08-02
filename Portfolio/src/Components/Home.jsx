4569b25ff8401478f74bf0ab60a9a50171500595
aathif12 <aathifahamad4@gmail.com>
2025-08-02 19:45:45 +0530

    Updated Cv

--- Portfolio/src/Components/Home.jsx
+++ Portfolio/src/Components/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
           </a>
         </div>
         <a
-          href="https://drive.google.com/file/d/1zQjoc-HpXn_A0T5VLUJQG4ONwHvaCQUS/view?usp=sharing"
+          href="https://drive.google.com/file/d/1vBWKytGeYgq9VvT8C1XQBhuvy_G6ODNy/view?usp=sharing"
           className="btn"
         >
           Download CV
